f114010a3523fbd0b3a05bbe0198a887985982dd
Enis Gunesen <egunesen@microsoft.com>
2018-02-06 21:49:21 +0100

    Add project files.

---
+++ TfsWebHookAspNet/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TfsWebHookAspNet.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TfsWebHookAspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TfsWebHookAspNet.WebApiApplication" Language="C#" %>
